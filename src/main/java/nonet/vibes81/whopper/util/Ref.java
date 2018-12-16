76f2d16865999f7f55266ecb3034b90c112501b8
Vibes81 <suprvibes@gmail.com>
2018-12-16 20:51:01 +0000

    Release 1.0.2

--- src/main/java/nonet/vibes81/whopper/util/Ref.java
+++ src/main/java/nonet/vibes81/whopper/util/Ref.java
@@ -5,7 +5,7 @@ public class Ref {
 
     public static final String MOD_ID = "whopper";
     public static final String NAME = "Main";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     public static final String CLIENT_PROXY_CLASS = "nonet.vibes81.whopper.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "nonet.vibes81.whopper.proxy.ServerProxy";
 
